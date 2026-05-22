7efe019d6827fe0daf6a5771fe7a6a976eeed938
Shailly Panwar <shaillypanwar7@gmail.com>
2026-05-22 23:46:26 +0530

    Added get routes for both web and mobile

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ const app = express();
 const PORT = 8000;
 
 const users=require('./MOCK_DATA.json'); //imports users data from the JSON file
-app.get('/users' ,(req,res)=>{
+app.get('/api/users' ,(req,res)=>{
     return res.json(users); //sends the users data as a JSON response
+});
+
+app.get('/users',(req,res)=>{
+    const html=`
+    <ul>
+        ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+     return res.send(html);
+});
+
+app.get("/api/users/:id",(req,res)=>{
+    const userid=Number(req.params.id); // Extracts the user ID from the request parameters and converts it to a number
+    const user=users.find((user)=>user.id===userid); // Searches for the user with the matching ID in the users array
+    return res.json(user);
 })
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+}); 
