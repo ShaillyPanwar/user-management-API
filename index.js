35fb73363ba59fefac7aca7b92bbf6eb8de376c5
Shailly Panwar <shaillypanwar7@gmail.com>
2026-05-23 20:02:32 +0530

    added post end point

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
+app.use(express.json()); // Middleware to parse JSON request bodies
 
 const users=require('./MOCK_DATA.json'); //imports users data from the JSON file
 app.get('/api/users' ,(req,res)=>{
@@ -20,6 +21,20 @@ app.get("/api/users/:id",(req,res)=>{
     const userid=Number(req.params.id); // Extracts the user ID from the request parameters and converts it to a number
     const user=users.find((user)=>user.id===userid); // Searches for the user with the matching ID in the users array
     return res.json(user);
+})
+
+
+app.post('/api/users',(req,res)=>{
+    const body=req.body;
+    users.push({
+        id:users.length+1,
+        first_name:body.first_name,
+        last_name:body.last_name,
+        email:body.email,
+        city:body.city,
+        gender:body.gender,
+    });
+    return res.json(users);
 })
 
 app.listen(PORT, () => {
